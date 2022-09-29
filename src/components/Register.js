cc20daea5bb2d0a82fc99975cb7a84409c900755
Nmohd <najeebu50@gmail.com>
2022-09-29 09:01:31 +0530

    book_client

---
+++ src/components/Register.js
@@ -0,0 +1,17 @@
+import Topheader from "./Topheader"
+import Header from "./Header"
+import Footer from "./Footer"
+
+const Register = () => {
+  return (
+    <>
+      <h1>Register Component</h1>
+      <Topheader />
+      <Header />
+      
+
+    </>
+  )
+}
+
+export default Register
---
+++ src/components/Topheader.js
@@ -0,0 +1,18 @@
+const Topheader = () => {
+  return (
+    <>
+      <div
+        style={{
+          background: "yellow",
+          padding: "5px",
+        }}
+      >
+        <h4 style={{ textAlign: "center" }}>
+          Resize the browser window to see the responsive effect.
+        </h4>
+      </div>
+    </>
+  )
+}
+
+export default Topheader
